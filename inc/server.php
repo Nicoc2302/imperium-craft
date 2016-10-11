b5873d011698340c3c9731268c36dc1233539568
Nicoc2302 <cronic@live.be>
2016-10-11 22:53:24 +0200

    image 1  presentation

--- inc/server.php
+++ inc/server.php
@@ -15,4 +15,7 @@
             Imperium-craft est un serveur survival-rp ambitieux de thème médiévo-fantastique moddé en 1.7.10 . Alliant architecture monumentale et réaliste, le serveur a pour but de vous immerger dans un monde nouveau et fantastique !  
         </p>
     </div>
+    <div class="col-md-6">
+        <img src="img/muraille.png">
+    </div>
 </div>
