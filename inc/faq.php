4f51bd3450000fc4a23d4a6d483d1cb3bcca2bf4
Nicoc2302 <cronic@live.be>
2016-10-11 22:25:09 +0200

    formulaire

--- inc/faq.php
+++ inc/faq.php
@@ -44,5 +44,30 @@
             Il n'y a pas de skin préconsus dans MPM, il faut impérativemetn que vous en choisissiez un enligne et de thème médiéval.<br>
         </p>
         <hr>
+        <form>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Email address</label>
+              <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+            </div>
+            <div class="form-group">
+              <label for="exampleInputPseudo">Pseudo</label>
+              <input type="text" class="form-control" id="exampleInputPseudo" placeholder="Pseudo">
+            </div>
+            <div class="form-group">
+              <label for="exampleInputProbleme">Probleme</label>
+              <input type="text" class="form-control" id="exampleInputProbleme" placeholder="Probleme">
+            </div>
+            <div class="form-group">
+              <label for="exampleInputMessage">Message</label>
+              <textarea class="form-control" id="exampleInputMessage" placeholder="Message"></textarea>
+            </div>
+            
+            <div class="checkbox">
+              <label>
+                <input type="checkbox"> Check me out
+              </label>
+            </div>
+            <button type="submit" class="btn btn-default">Submit</button>
+        </form>
     </div>
 </div>
