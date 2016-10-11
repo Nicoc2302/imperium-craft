d3af1eb2f32a1534b01604843f1e623d814c9169
Nicoc2302 <cronic@live.be>
2016-10-11 21:52:04 +0200

    faq 1ere

--- inc/faq.php
+++ inc/faq.php
@@ -12,20 +12,20 @@
             Voilà, vous êtes près à jouer !
         </p>
         <p>
-        <h2>
-            2) J'ai un écran bizarre quand je me connecte puis un léger freeze puis tout devient normal.
-        </h2>
-        Le serveur est équipé d'un ressource pack qui s'installe automatiquement lors de la connexion. Ceux qui le possèdent déjà n'y verront aucune différence tandis que les autres verront leur textures modifiées afin qu'ils aient le meilleur rendu possible des environements d'Imperium.
+            <h2>
+                2) J'ai un écran bizarre quand je me connecte puis un léger freeze puis tout devient normal.
+            </h2>
+            Le serveur est équipé d'un ressource pack qui s'installe automatiquement lors de la connexion. Ceux qui le possèdent déjà n'y verront aucune différence tandis que les autres verront leur textures modifiées afin qu'ils aient le meilleur rendu possible des environements d'Imperium.
         </p>
         <p>
             <h2>
                 3) Comment obtenir l'apparence de la race que j'ai choisi ? 
             </h2>
-            Pour changer d'apparence, Imperium-craft utilise le mod More Player Model de Noppes.<br>
-            - Pour changer d'apaprence, choisissez d'abord en surfant sur le net un skin en rapport avec votre race qui fasse médiéval.<br>
-            - Une fois le skin trouvé, rendez-vous en jeu et appuyez sur f12.<br>
-            Cette action ouvre le menu MPM qui vous permettra de changer d'apparence.<br>
-            - Une fois dans le menu, cliquez sur "Load Settings". Dans ce menu déroulant choissre votre race et votre sexe.<br>
+                Pour changer d'apparence, Imperium-craft utilise le mod More Player Model de Noppes.<br>
+                - Pour changer d'apaprence, choisissez d'abord en surfant sur le net un skin en rapport avec votre race qui fasse médiéval.<br>
+                - Une fois le skin trouvé, rendez-vous en jeu et appuyez sur f12.<br>
+                Cette action ouvre le menu MPM qui vous permettra de changer d'apparence.<br>
+                - Une fois dans le menu, cliquez sur "Load Settings". Dans ce menu déroulant choissre votre race et votre sexe.<br>
             <ul>
                 <li>pour les Humains, choisissez Human Male/Female puis load</li>
 
@@ -38,9 +38,11 @@
                 <li>pour les Lupus choississez Wolf Male/Female puis load</li>
 
                 <li>pour les Nagas, il faut rester dans le menu principale de MPM. une fois sur le menu, cliquez sur "Legs" a gauche de l'écran. Une fosi dans le menu Legs, cliquez plusieurs fois sur le premier bouton afin de tomber sur "Player Naga" ou tout simplement "Naga".</li>
-            </ul><br>
+            </ul>
+            <br>
             Une fois terminer, faites "exit"' !<br>
             Il n'y a pas de skin préconsus dans MPM, il faut impérativemetn que vous en choisissiez un enligne et de thème médiéval.<br>
         </p>
+        <hr>
     </div>
 </div>
