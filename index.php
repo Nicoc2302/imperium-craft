ef780b936457008746bfaf6a2bd00873d80556b5
project-tdev <development@tdev.be>
2016-10-11 23:07:21 +0200

    activate link for forum

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ and open the template in the editor.
                     <li class="active"><a href="?page=home">Home</a></li>
                     <li><a href="?page=server">Présentation</a></li>
                     <li><a href="?page=community">Communauté</a></li>
-                    <li><a href="#/forum">Forum</a></li>
+                    <li><a href="/forum">Forum</a></li>
                     <li><a href="?page=buy">Don et boutique</a></li>
                     <li><a href="?page=faq">FAQ</a></li>
                     <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Le serveur <span class="caret"></span></a>
