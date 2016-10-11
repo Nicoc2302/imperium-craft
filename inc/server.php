6685d4eff824c211895cc6df8dcc88fa7b785e3b
Nicoc2302 <cronic@live.be>
2016-10-11 23:11:44 +0200

    image 2  presentation

--- inc/server.php
+++ inc/server.php
@@ -19,3 +19,23 @@
         <img src="img/muraille.png">
     </div>
 </div>
+<div class="row">
+    
+    <div class="col-md-6">
+        <img src="img/perso.png">
+    </div>
+    <div class="col-md-6">
+        <p>
+            <h2>
+                Un gameplay unique
+            </h2>
+        Imperium-craft propose trois gameplay différents:<br>
+        - Citoyen<br>
+        - Voyageur<br>
+        - Soldat<br>
+        Ces trois catégories régissent l’entièreté de la pyramide socio-économique du serveur en interaction avec les pnjs et les quêtes.<br>
+        Chaque joueur se voit choisir sa « classe » au début de sa vie sur Imperium et la conservera jusqu’à sa mort ou jusqu’à ce qu’il décide d’en changer. En effet, les classes ne sont pas imperméables et un marchand peut décider de voyager comme il peut s’engager à l’armée et vice versa.<br>
+
+        </p>
+    </div>
+</div>
