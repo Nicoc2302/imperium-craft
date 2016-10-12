8f6fe0625a52f050647e2120b7dbfaf26af7eb21
Nicoc2302 <nicoc2302@air-de-nicolas.lan>
2016-10-12 14:32:27 +0200

    css

--- inc/server.php
+++ inc/server.php
@@ -1,123 +1,124 @@
-<div class="row">
-    <div class="col-md-6">
+<div id="server"
+    <div class="row">
+        <div class="col-md-6">
+        </div>
+        <div class="col-md-6"></div>
     </div>
-    <div class="col-md-6"></div>
-</div>
-<div class="row">
-    <div class="col-md-5 col-md-offset-1">
-        <p>
-            <h2>
-                Qu'est-ce qu'imperium ?
-            </h2>
-            Imperium-craft est un serveur survival-rp ambitieux de thème médiévo-fantastique moddé en 1.7.10 . Alliant architecture monumentale et réaliste, le serveur a pour but de vous immerger dans un monde nouveau et fantastique !  
-        </p>
+    <div class="row">
+        <div class="col-md-5 col-md-offset-1">
+            <p>
+                <h2>
+                    Qu'est-ce qu'imperium ?
+                </h2>
+                Imperium-craft est un serveur survival-rp ambitieux de thème médiévo-fantastique moddé en 1.7.10 . Alliant architecture monumentale et réaliste, le serveur a pour but de vous immerger dans un monde nouveau et fantastique !  
+            </p>
+        </div>
+        <div class="col-md-5">
+            <img src="img/muraille.png">
+        </div>
+        <div class="col-md-offset-1"></div>
     </div>
-    <div class="col-md-5">
-        <img src="img/muraille.png">
-    </div>
-    <div class="col-md-offset-1"></div>
-</div>
-<div class="row">
-    
-    <div class="col-md-5 col-md-offset-1">
-        <img src="img/perso.png">
-    </div>
-    <div class="col-md-5">
-        <p>
-            <h2>
-                Un gameplay unique
-            </h2>
-        Imperium-craft propose trois gameplay différents:<br>
-        - Citoyen<br>
-        - Voyageur<br>
-        - Soldat<br>
-        Ces trois catégories régissent l’entièreté de la pyramide socio-économique du serveur en interaction avec les pnjs et les quêtes.<br>
-        Chaque joueur se voit choisir sa « classe » au début de sa vie sur Imperium et la conservera jusqu’à sa mort ou jusqu’à ce qu’il décide d’en changer. En effet, les classes ne sont pas imperméables et un marchand peut décider de voyager comme il peut s’engager à l’armée et vice versa.<br>
+    <div class="row">
 
-        </p>
-    </div>
-    <div class="col-md-offset-1"></div>
-</div>
-<div class="row">
-    <div class="col-md-5 col-md-offset-1">
-        <p>
-            <h4>
-                1. Les citoyens
-            </h4>
-            Au début de son aventure, chaque citoyen se voit s’offrir la possibilité de rejoindre l’une des nombreuses guildes de métier que compte la cité. La plus prestigieuse d’entre-elles étant la guilde de Mages. En incorporant une guilde, chaque joueur entre dans un cercle d’entraide et de camaraderie qui le guidera tout au long de sa vie sur imperium.<br>
-            Cependant, la vie de la classe citoyenne ne s’arrête pas au travail dans une guilde ! Les joueurs ont accès à la vie politique du royaume et beaucoup d’entre-eux se sont vu confié des missions de colonisation faisant d’eux des héros nationaux.
+        <div class="col-md-5 col-md-offset-1">
+            <img src="img/perso.png">
+        </div>
+        <div class="col-md-5">
+            <p>
+                <h2>
+                    Un gameplay unique
+                </h2>
+            Imperium-craft propose trois gameplay différents:<br>
+            - Citoyen<br>
+            - Voyageur<br>
+            - Soldat<br>
+            Ces trois catégories régissent l’entièreté de la pyramide socio-économique du serveur en interaction avec les pnjs et les quêtes.<br>
+            Chaque joueur se voit choisir sa « classe » au début de sa vie sur Imperium et la conservera jusqu’à sa mort ou jusqu’à ce qu’il décide d’en changer. En effet, les classes ne sont pas imperméables et un marchand peut décider de voyager comme il peut s’engager à l’armée et vice versa.<br>
 
-        </p>
+            </p>
+        </div>
+        <div class="col-md-offset-1"></div>
     </div>
-    <div class="col-md-5">
-        <img src="img/armurerie.png">
-    </div>
-    <div class="col-md-offset-1"></div>
-</div>
-<div class="row">
-    
-    <div class="col-md-5 col-md-offset-1">
-        <img src="img/pilbateau.png">
-    </div>
-    <div class="col-md-5">
-        <p>
-            <h4>
-                2. Les voyageurs
-            </h4>
-            Les voyageurs sont des nomades et surtout des explorateurs. Hors des contraintes d’une vie dictée par l’économie et les impôts, les auberges sont leur foyer. Les voyageurs n’ont, pour la plupart pas de logement fixe, et se voient parfois refuser l’accès à des villes à cheval sur la question des vagabonds.<br>
-            Ne figurant sur aucun registre et donc insaisissable, certains servent la guilde des assassins ou des voleurs, mais la majorité servent la veuve et l’orphelin. Lorsqu’ils n’arpentent pas les grottes et autres donjons que recèle le monde, ils peuvent être engagés par à peu près tout le monde, surtout les guildes des mages ou des alchimistes.
-        </p>
-    </div>
-    <div class="col-md-offset-1"></div>
-</div>
-<div class="row">
-    <div class="col-md-5 col-md-offset-1">
-        <p>
-            <h4>
-                1. Les militaires
-            </h4>
-            Cette classe est le bras armé du royaume. Chaques joueurs qui souhaitent se battre dans les rangs de l’armée doivent subir un entraînement intense pour pouvoir gravir les échelons hiérarchiques. Les militaires résident dans les casernes, ne payent pas d’impôts et peuvent être mobilisés à chaque instant. Il n’est pas rare qu’un soldat soit caserné à plusieurs forts différents sur une semaine de temps.<br>
-            Que ce soit la garde, l’armée ou la marine, chaque joueur qui intègre les forces armées du royaume est traité avec le respect et l’admiration que la société Adelonienne leur a toujours portée.<br>
-            Mais être militaire ce n’est pas seulement suivre les ordres, en donné où casser des têtes. La haute hiérarchie est souvent amenée, lorsqu’ils souhaitent se retirer de l’armée, à obtenir des postes administratifs importants au sein du royaume, allant de Chancelier suprême à conseiller militaire personnel du roi.
-        </p>
-    </div>
-    <div class="col-md-5">
-        <img src="img/gardemur.png">
-    </div>
-    <div class="col-md-offset-1"></div>
-</div>
-<div class="row">
-    
-    <div class="col-md-5 col-md-offset-1">
-        <img src="img/taverne.png">
-    </div>
-    <div class="col-md-5">
-        <p>
-            <h2>
-                Un monde qui change
-            </h2>
-            Une Histoire faite PAR les joueurs POUR les joueurs !<br>
-            Le Monde d'Imperium est un monde vivant ou chaque acte à des conséquences. Selon l'action des joueurs ou des maîtres du jeu, les événements peuvent prendre une tournure imprévisible, allant d'une révolution à des guerres entre villes secondaires jusqu'à même la sécession de certaines villes afin de former un autre royaume !<br><br>
-            Les citoyens ne sont pas écartés du pouvoir, au contraire. jusqu'à 80 joueurs peuvent sièger au Sénat ou se prennent toutes les décisions concernant la vie du royaume, les lois et la création de nouvelles villes.<br><br>
-            L'histoire d'Imperium est entre les mains des joueurs !
-        </p>
-    </div>
-    <div class="col-md-offset-1"></div>
-</div>
-<div class="row">
-    <div class="col-md-6 col-md-offset-3">
-        <p>
-            <h2>
-                Un staff qui recrute
-            </h2>
-        Le Staff c'est un fonda et deux admins qui recrutent !<br>
-        Nous avons besoin de modérateurs, d'animateurs, mais surtout de joueurs qui en veulent!<br><br>
-        Pour rejoindre le staff une solution postuler sur le site à travers la plateforme contactes-nous sur la page principale du Site !<br><br>
-        A bientîot, sur Imperium !<br>
-        Fondateur Tombraxis.
+    <div class="row">
+        <div class="col-md-5 col-md-offset-1">
+            <p>
+                <h4>
+                    1. Les citoyens
+                </h4>
+                Au début de son aventure, chaque citoyen se voit s’offrir la possibilité de rejoindre l’une des nombreuses guildes de métier que compte la cité. La plus prestigieuse d’entre-elles étant la guilde de Mages. En incorporant une guilde, chaque joueur entre dans un cercle d’entraide et de camaraderie qui le guidera tout au long de sa vie sur imperium.<br>
+                Cependant, la vie de la classe citoyenne ne s’arrête pas au travail dans une guilde ! Les joueurs ont accès à la vie politique du royaume et beaucoup d’entre-eux se sont vu confié des missions de colonisation faisant d’eux des héros nationaux.
 
-        </p>
+            </p>
+        </div>
+        <div class="col-md-5">
+            <img src="img/armurerie.png">
+        </div>
+        <div class="col-md-offset-1"></div>
     </div>
-    <div class="col-md-3">
+    <div class="row">
+
+        <div class="col-md-5 col-md-offset-1">
+            <img src="img/pilbateau.png">
+        </div>
+        <div class="col-md-5">
+            <p>
+                <h4>
+                    2. Les voyageurs
+                </h4>
+                Les voyageurs sont des nomades et surtout des explorateurs. Hors des contraintes d’une vie dictée par l’économie et les impôts, les auberges sont leur foyer. Les voyageurs n’ont, pour la plupart pas de logement fixe, et se voient parfois refuser l’accès à des villes à cheval sur la question des vagabonds.<br>
+                Ne figurant sur aucun registre et donc insaisissable, certains servent la guilde des assassins ou des voleurs, mais la majorité servent la veuve et l’orphelin. Lorsqu’ils n’arpentent pas les grottes et autres donjons que recèle le monde, ils peuvent être engagés par à peu près tout le monde, surtout les guildes des mages ou des alchimistes.
+            </p>
+        </div>
+        <div class="col-md-offset-1"></div>
     </div>
-</div>
+    <div class="row">
+        <div class="col-md-5 col-md-offset-1">
+            <p>
+                <h4>
+                    1. Les militaires
+                </h4>
+                Cette classe est le bras armé du royaume. Chaques joueurs qui souhaitent se battre dans les rangs de l’armée doivent subir un entraînement intense pour pouvoir gravir les échelons hiérarchiques. Les militaires résident dans les casernes, ne payent pas d’impôts et peuvent être mobilisés à chaque instant. Il n’est pas rare qu’un soldat soit caserné à plusieurs forts différents sur une semaine de temps.<br>
+                Que ce soit la garde, l’armée ou la marine, chaque joueur qui intègre les forces armées du royaume est traité avec le respect et l’admiration que la société Adelonienne leur a toujours portée.<br>
+                Mais être militaire ce n’est pas seulement suivre les ordres, en donné où casser des têtes. La haute hiérarchie est souvent amenée, lorsqu’ils souhaitent se retirer de l’armée, à obtenir des postes administratifs importants au sein du royaume, allant de Chancelier suprême à conseiller militaire personnel du roi.
+            </p>
+        </div>
+        <div class="col-md-5">
+            <img src="img/gardemur.png">
+        </div>
+        <div class="col-md-offset-1"></div>
+    </div>
+    <div class="row">
+
+        <div class="col-md-5 col-md-offset-1">
+            <img src="img/taverne.png">
+        </div>
+        <div class="col-md-5">
+            <p>
+                <h2>
+                    Un monde qui change
+                </h2>
+                Une Histoire faite PAR les joueurs POUR les joueurs !<br>
+                Le Monde d'Imperium est un monde vivant ou chaque acte à des conséquences. Selon l'action des joueurs ou des maîtres du jeu, les événements peuvent prendre une tournure imprévisible, allant d'une révolution à des guerres entre villes secondaires jusqu'à même la sécession de certaines villes afin de former un autre royaume !<br><br>
+                Les citoyens ne sont pas écartés du pouvoir, au contraire. jusqu'à 80 joueurs peuvent sièger au Sénat ou se prennent toutes les décisions concernant la vie du royaume, les lois et la création de nouvelles villes.<br><br>
+                L'histoire d'Imperium est entre les mains des joueurs !
+            </p>
+        </div>
+        <div class="col-md-offset-1"></div>
+    </div>
+    <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+            <p>
+                <h2>
+                    Un staff qui recrute
+                </h2>
+            Le Staff c'est un fonda et deux admins qui recrutent !<br>
+            Nous avons besoin de modérateurs, d'animateurs, mais surtout de joueurs qui en veulent!<br><br>
+            Pour rejoindre le staff une solution postuler sur le site à travers la plateforme contactes-nous sur la page principale du Site !<br><br>
+            A bientîot, sur Imperium !<br>
+            Fondateur Tombraxis.
+
+            </p>
+        </div>
+        <div class="col-md-3">
+        </div>
+    </div>
