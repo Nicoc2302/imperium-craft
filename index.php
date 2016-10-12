70902bdd35f2a6ff5675c8ff22547420718d9196
Nicoc2302 <nicoc2302@air-de-nicolas.lan>
2016-10-12 14:10:50 +0200

    container

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ and open the template in the editor.
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     </head>
     <body>
-        <div class="container-fluid">
+        <div class="container">
             <header class="masthead">
                 <img src="img/logo.png">
                 <h3 class="text-muted">Imperium-Craft</h3>
