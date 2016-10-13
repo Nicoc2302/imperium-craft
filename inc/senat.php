babe6e7fd41375e194f4fcf3f3b703b0ab1e481f
Nicoc2302 <nicoc2302@air-de-nicolas.lan>
2016-10-13 13:13:36 +0200

    Sénat page

--- inc/senat.php
+++ inc/senat.php
@@ -1,7 +1,22 @@
 <div class="row">
     <div class="col-md-12">
         <p>
-            <div class="alert alert-warning" role="alert">Senat à venir</div>
+            <h2>Le Sénat.</h2>
+
+            Selon les périodes, des élections auront lieux pour élire les différents sénateurs et autres magistrats qui siègeront aux côtés des magistrats permanents.<br>
+            Le Sénat se trouve sur la colline du Purgatoire au nord de la ville.<br>
+            Ils y a plusieurs sortes de magistrats :<br>
+            <ul>
+                <li>Sénateur : le sénateur est nommé tous les deux mois. Ils représentent le peuple dans son intégralité. Tout le monde peut se présenter comme sénateur, mais il est interdit de cumuler deux mandats à la suite. chaque sénateur possède une voix au sénat et peut créer des projets de loi.</li> 
+                <li>Représentants du peuple : Ces magistrats représentent chacun un domaine spécifique.
+                    -Représentant des finances
+                    -Représentant des armées
+                    -Représentant des infrastructures
+                    -Représentant des guildes (élu par les chefs de guilde)</li>
+
+                <li>Le Roi possède un droit de véto et peut créer des projets de loi.</li>
+            </ul>
         </p>
     </div>
+    <div class="col-md-12"><img src="./img/taverne.png"></div>
 </div>
