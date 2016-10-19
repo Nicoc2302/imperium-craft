cb445dec7f71d78ac87656d5da9bc2f466e46d2f
Nicoc2302 <cronic@live.be>
2016-10-19 22:28:42 +0200

    icone

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ and open the template in the editor.
         <title>Imperium-Craft</title>
         <link href="css/bootstrap.min.css" rel="stylesheet">
         <link href="css/custom.css" rel="stylesheet">
-        <link rel="icon" type="image/png" href="logo.png" />
+        <link rel="icon" type="image/png" href="img/logo.png" />
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     </head>
     <body>
