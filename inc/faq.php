cd61b265cc179a688ddde392527740372cf79c0d
project-tdev <development@tdev.be>
2016-10-19 14:19:26 +0200

    best way to include form

--- inc/faq.php
+++ inc/faq.php
@@ -46,30 +46,8 @@
         <hr>
     </div>
     <div class="col-md-4">
-        <form>
-            <div class="form-group">
-              <label for="exampleInputEmail1">Adresse Email</label>
-              <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
-            </div>
-            <div class="form-group">
-              <label for="exampleInputPseudo">Pseudo</label>
-              <input type="text" class="form-control" id="exampleInputPseudo" placeholder="Pseudo">
-            </div>
-            <div class="form-group">
-              <label for="exampleInputProbleme">Probleme</label>
-              <input type="text" class="form-control" id="exampleInputProbleme" placeholder="Probleme">
-            </div>
-            <div class="form-group">
-              <label for="exampleInputMessage">Message</label>
-              <textarea class="form-control" id="exampleInputMessage" placeholder="Message"></textarea>
-            </div>
-            
-            <div class="checkbox">
-              <label>
-                <input type="checkbox"> Check me out
-              </label>
-            </div>
-            <button type="submit" class="btn btn-default">Submit</button>
-        </form>
+        <?php
+        include_once 'inc/php/form_contact.php';
+        ?>
     </div>
 </div>
---
+++ inc/php/form_contact.php
@@ -0,0 +1,26 @@
+
+        <form>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Adresse Email</label>
+              <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+            </div>
+            <div class="form-group">
+              <label for="exampleInputPseudo">Pseudo</label>
+              <input type="text" class="form-control" id="exampleInputPseudo" placeholder="Pseudo">
+            </div>
+            <div class="form-group">
+              <label for="exampleInputProbleme">Probleme</label>
+              <input type="text" class="form-control" id="exampleInputProbleme" placeholder="Probleme">
+            </div>
+            <div class="form-group">
+              <label for="exampleInputMessage">Message</label>
+              <textarea class="form-control" id="exampleInputMessage" placeholder="Message"></textarea>
+            </div>
+            
+            <div class="checkbox">
+              <label>
+                <input type="checkbox"> Check me out
+              </label>
+            </div>
+            <button type="submit" class="btn btn-default" disabled="true">Submit</button>
+        </form>
