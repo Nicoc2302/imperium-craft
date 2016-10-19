cf11154d8f2b0b3a1603a57cb988de948d662713
Nicoc2302 <cronic@live.be>
2016-10-19 23:46:33 +0200

    pagination

--- inc/home.php
+++ inc/home.php
@@ -41,8 +41,9 @@
             <?php
             //Define number of pages
                 $nbp = count($lines)%5 ==0 ? count($lines)/NBLINE:(ceil(count($lines)/NBLINE));// 10%5 = 0 => 10/5 = 2 // 11%5 == 1 => (11/5)+1
+                $previous = $_GET['start'];
             //iterate every page
-                echo'<li><a href="?page=home&start='.$start.'">Previous</a></li>';
+                echo'<li><a href="?page=home&start='.$previous.'">Previous</a></li>';
                 for($num=1;$num<=$nbp;$num++)// 1<=2
                 {
                     echo'<li><a href="?page=home&start='.$num.'">'.$num.'</a></li>';
