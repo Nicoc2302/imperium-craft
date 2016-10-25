9b27f476eed9c916050b2c54a744ae5e796d4980
Nicoc2302 <nicoc2302@air-de-nicolas.lan>
2016-10-25 08:03:23 +0200

    html city

--- inc/city.php
+++ inc/city.php
@@ -1,7 +1,42 @@
-<div class="row">
-    <div class="col-md-12">
+<section class="row">
+    <article class="col-md-12">
+        <h2>
+            La Première race.
+        </h2>
         <p>
-            <div class="alert alert-warning" role="alert">La cite d'Adelon à venir</div>
+            Le premier continent créé par les dieux était le continent aride d’Edrévor. Sur ce dernier, la déesse Tobalt, ennuyée de régner seulement sur des animaux, décida de créer une race d’être supérieurs : les Elfes.<br>
+            Pour les guidés, elle créa une seconde race exclusivement féminine, les Naïades, afin qu’elles enseignent la Voie des Dieux aux Elfes.<br>
+            Cette voie des dieux était régie par trois ordres : l’ordre magique, l’ordre éthique et l’ordre technique. La fusion de ces trois ordres permit dès lors aux Elfes de prospérer et de devenir une civilisation brillante et pacifique jusqu’au jour où, venu d’un autre monde, une force obscure se mit à se répendre depuis le nord du contient.<br>
+            On raconte que c’est après le passage d’une flamme dans le ciel que le fléau se rependit et avec lui, la cupidité, la guerre et la mort. Les Elfes finirent par s’entretuer et peu d’entre eux subsistèrent.<br>
+            Tobalt, profondément attristée par le déclin de sa progéniture décida de se sacrifier afin de chasser le fléau et sauver sa création en divisant son âme en six parties qui se rependirent sur le monde chassant les ténèbres. Après le sacrifice, les Elfes se rassemblèrent et reformèrent leur civilisation, cependant le fléau avait laissé une marque indélébile chez certains Elfes : la cupidité, manifestée par le sceau noir.<br>
         </p>
-    </div>
-</div>
+        
+    </article>
+    <article class="col-md-12">
+        <h2>
+            L’Efflorescence
+        </h2>
+        <p>
+            Bien que le fléau ait disparu, certains Elfes marqués du sceau continuèrent de semer le trouble et une nouvelle guerre éclata. Les Elfes Scétélaires (marqués du sceau) étant peu nombreux, la guerre se termina avec leur massacre quasi-total et vit l’avènement d’un Empire gigantesque : le Dominion Elfique.<br>
+            Plusieurs millénaires passèrent et comme par miracle, des mages retrouvèrent un fragment d’âme de Tobalt dans un ancien temple Elfe. Ils le ramenèrent à la Capitale du Dominion et émirent l’hypothèse qu’ils pourraient ramener Tobalt à la vie en rassemblant les fragments. Les Elfes se mirent alors à la recherche de ces derniers et il ne fut pas difficile de les retrouvés sur ce continent aride ou seuls cinq lieux étaient dotés d’une efflorescence de vie dont l’un se trouvait être l’endroit où le premier fragment avait été retrouvé.<br>
+            Cependant ils découvrirent rapidement que les emplacements étaient farouchement gardés hormis celui qui avait été enfui dans leur frontières. En effet, un touchant le sol stérile des sites d’impact, les fragments libérèrent la volonté de Tobalt recréant toute sortes d’êtres vivants dont des êtres intelligents qui prirent le fragment comme relique sacrée.<br>
+            Parmi ces races nouvelles, on compte les Humains, les Arkoudas, les Nains, les Nagas et les Lupus. Même si leur mode de création était similaire, ces races partageaient un trait commun inconnu chez les Elfes. Lors de leur création, la lumière et les ténèbres se mélangèrent donnant à ces races des caractères inhérents aux ténèbres ainsi qu’à la lumière.<br>
+        </p>
+        
+    </article>
+    <article class="col-md-12">
+        <h2>
+            Le Désastre
+        </h2>
+        <p>
+            Horrifiés de voir que le sacrifice de leur déesse n’avait en réalité qu’encrer les ténèbres dans de nouvelles races, les Elfes décidèrent de les éradiqués, mais se heurtèrent aux Naïades qui, en tant que prêtresses de Tobalt, ne pouvaient en aucun cas les laisser détruire ses ultimes créations. Soumis aux enseignements des Naïades, les Elfes laissèrent les nouvelles races en vie sans renoncer à leur prendre les fragments.<br><br>
+            S’ils ne pouvaient détruire les races et reprendre les fragments par eux même, ils usèrent de subterfuges pour que les jeunes races se fassent la guerre et, dans la confusion, leur prirent les fragments.<br><br>
+            Une fois réunis, les Elfes tentèrent de réassembler les fragments, mais le résultat ne fut pas celui attendu. En les réunissant, les Elfes déchainèrent la Lumière, mais aussi les Ténèbres absorbés lors du sacrifice. Ces deux énergies combinées corrompirent les Elfes et les réduisirent au même rang que les autres races.<br><br>
+            Non contentes d’avoir détrôné les Elfes en leur retirant leur pureté, les énergies produisirent de puissants tremblements de terre séparant le super continent en multiples fragments réduisant les royaumes en miettes et noyant les côtes au moyen de puissants tsunamis.<br><br>
+            Après le désastre, chaque race ayant été mise sur un pied d’égalité, seules les Naïades demeuraient encore totalement pures. A travers elles, fut à nouveau transmise la Voie des Dieux et une nouvelle civilisation ou chaque race pouvait vivre en harmonie vit le jour en la Cité d’Adelon.<br><br>
+            Seule comme le soleil dans le ciel, la civilisation Adonienne se donna comme objectif de repeupler le monde. Un monde encore inexploré.<br>
+        </p>
+        
+    </article>
+    
+</section>
