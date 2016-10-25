155e88b477df810abe04eac3a156cb8fee3c4784
Nicoc2302 <nicoc2302@air-de-nicolas.lan>
2016-10-25 16:12:57 +0200

    administration

--- inc/administation.php
+++ inc/administation.php
@@ -1,7 +1,37 @@
-<div class="row">
-    <div class="col-md-12">
-        <p>
-            <div class="alert alert-warning" role="alert">Administration a venir.</div>
-        </p>
-    </div>
+<div class="col-md-4">
+    <form method="post">
+        <div class="form-group">
+          <label for="exampleInputidentifiants">Identifiants</label><br>
+          <input type="text" class="form-control" id="exampleInputidentifiants" placeholder="Identifiants" required name = "Identifiant">
+        </div>
+        <div class="form-group">
+            <label for="exampleInputpassword">Mot de passe</label><br>
+          <input type="password" class="form-control" id="exampleInputpassword" placeholder="password" required name = "password">
+        </div>
+
+        <input type="submit" class="btn btn-default"></input>
+    </form>
 </div>
+<?php
+    $filename = "files/root.txt";
+    if($_SERVER['REQUEST_METHOD']=='POST'){
+        $identifiant = $_POST['Identifiant'];
+        $password = $_POST['password'];
+        $lines = file($filename);
+        $lines = array_reverse($lines);
+        $start = 0;
+        define('NBLINE', 5);
+        if(isset($_GET['start']) && (intval($_GET['start']))){
+            $start = $_GET['start'];
+            $start = $start>0 ? ($start-1) *NBLINE:0;
+        }
+
+        for($i=$start;$i<$start + NBLINE && $i<count($lines);$i++)
+        {
+            $split =explode("|", $lines[$i]);
+            echo "<h1>$split[0]</h1>";
+            echo "<p>$split[1]<p>";
+        }
+?>
+   
+
